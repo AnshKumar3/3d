da50a644d492dff8ce1c735155b03ddc4790ea0c
unknown <vaibhavchaurasia768@gmail.com>
2024-04-02 10:11:40 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState,Suspense } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Canvas } from '@react-three/fiber';
+import {Environment, OrbitControls} from '@react-three/drei';
+import Earth from '../public/Earth';
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Canvas>
+      <ambientLight />
+      <OrbitControls />
+      <Suspense fallback={null}>
+<Earth />
+      </Suspense>
+      <Environment preset='sunset'/>
+    </Canvas>
+    </>
+  )
+}
+
+export default App
