118f1a8bebbbe0b2b0a8bb250a3c410e345a9681
unknown <vaibhavchaurasia768@gmail.com>
2024-04-03 07:32:09 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       <ambientLight />
       <OrbitControls 
           autoRotate
-          enableZoom={false}
+          enableZoom={true}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}/>
       <Suspense fallback={null}>
