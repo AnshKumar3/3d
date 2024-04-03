62f3b9817d1fc60ad14cc5af9f34a5a41359a1fa
Ansh Kumar <88948265+AnshKumar3@users.noreply.github.com>
2024-04-03 19:18:59 +0000

    fix

--- public/Scene.jsx
+++ public/Scene.jsx
@@ -13,27 +13,10 @@ import { useGLTF } from '@react-three/drei'
 export default function Model(props) {
   const { nodes, materials } = useGLTF('/scene.gltf')
   
-  setCurrentStage
   
-  const normalizedRotation = ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
 
 // Set the current stage based on the island's orientation
-switch (true) {
-  case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
-    setCurrentStage(4);
-    break;
-  case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
-    setCurrentStage(3);
-    break;
-  case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
-    setCurrentStage(2);
-    break;
-  case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
-    setCurrentStage(1);
-    break;
-  default:
-    setCurrentStage(null);
-}
+
   
   return (
     <group {...props} dispose={null}>
