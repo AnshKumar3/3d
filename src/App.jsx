342c0b1b195177ecfd0873c28e24590412605b83
unknown <vaibhavchaurasia768@gmail.com>
2024-04-03 07:35:56 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
        fov: 45,
        near: 0.1,
        far: 200,
-       position: [-4, 3, 6],
+       position: [-4, 3, 30],
      }}>
       <ambientLight />
       <OrbitControls 
